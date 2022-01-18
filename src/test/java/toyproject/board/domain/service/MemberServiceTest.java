4d1bc3ded8fadf58e8fd929e1217a58836e16754
minhwan <jeff970069@gmail.com>
2022-01-18 09:55:57 +0900

    githubAction testcode
    githubAction push testcode

--- src/test/java/toyproject/board/domain/service/MemberServiceTest.java
+++ src/test/java/toyproject/board/domain/service/MemberServiceTest.java
@@ -18,43 +18,43 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @Transactional
 class MemberServiceTest {
 
-//    @Autowired
-//    MemberService memberService;
-//    @Autowired
-//    MemberRepository memberRepository;
-//
-//    @Test
-//    void 회원_가입(){
-//    //given
-//        Member member = Member.builder()
-//                .username("mins")
-//                .password("1234")
-//                .email("email")
-//                .build();
-//    //when
-//        Long joinId = memberService.join(member);
-//        //then
-//        Assertions.assertEquals(member, memberRepository.findOne(joinId));
-//    }
-//
-//    @Test
-//    @Rollback
-//    void 중복_회원_가입 (){
-//    //given
-//        Member member = Member.builder()
-//                .username("min")
-//                .password("1234")
-//                .email("email")
-//                .build();
-//
-//        Member member2 = Member.builder()
-//                .username("min")
-//                .password("1234")
-//                .email("email")
-//                .build();
-//    //when
-//        memberService.join(member);
-//    //then
-//        assertThrows(IllegalStateException.class , () -> memberService.join(member2));
-//    }
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원_가입(){
+    //given
+        Member member = Member.builder()
+                .username("mins")
+                .password("1234")
+                .email("email")
+                .build();
+    //when
+        Long joinId = memberService.join(member);
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(joinId));
+    }
+
+    @Test
+    @Rollback
+    void 중복_회원_가입 (){
+    //given
+        Member member = Member.builder()
+                .username("min")
+                .password("1234")
+                .email("email")
+                .build();
+
+        Member member2 = Member.builder()
+                .username("min")
+                .password("1234")
+                .email("email")
+                .build();
+    //when
+        memberService.join(member);
+    //then
+        assertThrows(IllegalStateException.class , () -> memberService.join(member2));
+    }
 }
