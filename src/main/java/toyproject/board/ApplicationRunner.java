98e000cdaea6e31cf7c544352637fda6b6150d75
minhwan <jeff970069@gmail.com>
2022-06-07 20:38:54 +0900

    mac project

--- src/main/java/toyproject/board/ApplicationRunner.java
+++ src/main/java/toyproject/board/ApplicationRunner.java
@@ -1,6 +1,5 @@
 package toyproject.board;
 
-import javafx.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
